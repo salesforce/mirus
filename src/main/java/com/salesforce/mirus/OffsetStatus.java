0d3635a76508d82d2a97e4fb8f4ea4f2a60f36fb
Paul Davidson <pdavidson@salesforce.com>
2018-10-26 10:46:28 -0700

    Use system line separator

--- src/main/java/com/salesforce/mirus/OffsetStatus.java
+++ src/main/java/com/salesforce/mirus/OffsetStatus.java
@@ -75,7 +75,7 @@ public class OffsetStatus {
           .forEach(
               k ->
                   System.out.printf(
-                      "%s\t%d\t%d\t%d\n",
+                      "%s\t%d\t%d\t%d" + System.lineSeparator(),
                       k.topic(), k.partition(), beginningOffsets.get(k), endOffsets.get(k)));
     }
   }
